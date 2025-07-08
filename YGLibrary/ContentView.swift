1f791bee02cac5e6f123829e179b37e4fb0c1625
sb-yjlim <yjlim@soft-berry.com>
2025-07-08 22:46:53 +0900

    Initial Commit

---
+++ YGLibrary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YGLibrary
+//
+//  Created by 임영준 on 7/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YGLibrary/YGLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  YGLibraryApp.swift
+//  YGLibrary
+//
+//  Created by 임영준 on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct YGLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
